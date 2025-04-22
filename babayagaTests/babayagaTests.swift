537e4b5349c9f082bb7355e0d220c8fddd0dbac8
nhagss <yagosouzaramos1@gmail.com>
2025-04-22 09:10:59 -0300

    Initial Commit

---
+++ babayagaTests/babayagaTests.swift
@@ -0,0 +1,17 @@
+//
+//  babayagaTests.swift
+//  babayagaTests
+//
+//  Created by Yago Souza Ramos on 4/22/25.
+//
+
+import Testing
+@testable import babayaga
+
+struct babayagaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
