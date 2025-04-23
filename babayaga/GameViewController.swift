acc2393cd56796e4f8b806d6b0110e2b14ec4539
Honório Filho <honoriovitor.web@gmail.com>
2025-04-23 10:51:14 -0300

    Organização de Pastas e Integração de Mecânica de Jogo

--- babayaga/GameViewController.swift
+++ babayaga/GameViewController.swift
@@ -7,40 +7,79 @@
 
 import UIKit
 import SpriteKit
-import GameplayKit
 
 class GameViewController: UIViewController {
-
+    
+    var spriteKitView = SKView()
+    let scene = GameScene()
+    
+    var changeDirectionBTN: UIButton! = {
+        let btn = UIButton()
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        btn.backgroundColor = .gray
+        return btn
+    }()
+    
+    var changePlanetButton: UIButton! = {
+        let btn = UIButton()
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        btn.backgroundColor = .gray
+        return btn
+    }()
+    
+    @objc func changeDirection(_ sender: UIButton) {
+        scene.changeDirection(planet: scene.planets[scene.currentPlanetIndex])
+    }
+    
+    @objc func changePlanet(_ sender: UIButton) {
+        scene.jumpOrChangePlanet()
+        changePlanetButton.setTitle("\(scene.currentPlanetIndex)", for: .normal)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
-        // including entities and graphs.
-        if let scene = GKScene(fileNamed: "GameScene") {
-            
-            // Get the SKScene from the loaded GKScene
-            if let sceneNode = scene.rootNode as! GameScene? {
-                
-                // Copy gameplay related content over to the scene
-                sceneNode.entities = scene.entities
-                sceneNode.graphs = scene.graphs
-                
-                // Set the scale mode to scale to fit the window
-                sceneNode.scaleMode = .aspectFill
-                
-                // Present the scene
-                if let view = self.view as! SKView? {
-                    view.presentScene(sceneNode)
-                    
-                    view.ignoresSiblingOrder = true
-                    
-                    view.showsFPS = true
-                    view.showsNodeCount = true
-                }
-            }
-        }
+        view.backgroundColor = .black
+        
+        scene.size = view.bounds.size
+        scene.scaleMode = .aspectFill
+        spriteKitView.presentScene(scene)
+        spriteKitView.frame = view.bounds
+        
+        //spriteKitView.scene?.backgroundColor = .black
+        //        spriteKitView.layer.position = CGPoint(x: view.bounds.midX, y: view.bounds.midY - 100)
+        
+        view.addSubview(spriteKitView)
+        view.addSubview(changeDirectionBTN)
+        view.addSubview(changePlanetButton)
+        
+        #if DEBUG
+        spriteKitView.ignoresSiblingOrder = true
+        spriteKitView.showsFPS = true
+        spriteKitView.showsNodeCount = true
+        #endif
+        
+        
+        changeDirectionBTN.addTarget(self, action: #selector(changeDirection), for: .touchUpInside)
+        changeDirectionBTN.layer.zPosition = 10
+        NSLayoutConstraint.activate([
+            changeDirectionBTN.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.2),
+            changeDirectionBTN.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.1),
+            changeDirectionBTN.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100),
+            changeDirectionBTN.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -100),
+        ])
+        
+        changePlanetButton.addTarget(self, action: #selector(changePlanet), for: .touchUpInside)
+        //        changePlanetButton.layer.zPosition = 10
+        NSLayoutConstraint.activate([
+            changePlanetButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.2),
+            changePlanetButton.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.1),
+            changePlanetButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100),
+            changePlanetButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 100),
+        ])
     }
-
+    
+    // Funções de orientação e status bar (mantidas como antes)
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         if UIDevice.current.userInterfaceIdiom == .phone {
             return .allButUpsideDown
@@ -48,8 +87,12 @@ class GameViewController: UIViewController {
             return .all
         }
     }
-
+    
     override var prefersStatusBarHidden: Bool {
         return true
     }
+}
+
+#Preview {
+    GameViewController()
 }
---
+++ babayaga/Model/PhysicsCategory.swift
@@ -0,0 +1,13 @@
+//
+//  PhysicsCategory.swift
+//  babayaga
+//
+//  Created by user on 23/04/25.
+//
+
+import Foundation
+
+struct PhysicsCategory {
+    static let player: UInt32 = 0x1 << 0
+    static let stairs: UInt32 = 0x1 << 1
+}
