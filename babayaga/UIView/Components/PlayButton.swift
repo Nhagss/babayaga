13fdf7d14da59fe5ecfc9e35ee0fce4d020c328e
João Roberto Fernandes Magalhães <120526440+joaorbrto@users.noreply.github.com>
2025-05-02 11:18:49 -0300

    🖼️ Added Components

---
+++ babayaga/UIView/Components/PlayButton.swift
@@ -0,0 +1,20 @@
+//
+//  PlayButton.swift
+//  babayaga
+//
+//  Created by Joao Roberto Fernandes Magalhaes on 02/05/25.
+//
+
+import SwiftUI
+
+struct PlayButton: View {
+    var body: some View {
+        ZStack{
+            
+        }
+    }
+}
+
+#Preview {
+    PlayButton()
+}
