64012f57e204bee73749b932c283d8c09bc460c4
João Roberto Fernandes Magalhães <120526440+joaorbrto@users.noreply.github.com>
2025-05-07 17:25:03 -0300

    interface completa

--- babayaga/UIView/Views/SettingsView.swift
+++ babayaga/UIView/Views/SettingsView.swift
@@ -8,53 +8,70 @@
 import SwiftUI
 
 struct SettingsView: View {
-    
+
     @State private var buttonFrames: [CGRect] = Array(repeating: .zero, count: 6)
-    
+    let assets : [String] = ["headphone", "music","haptics","restore", "noads", "oppelt" ]
     var body: some View {
         ZStack {
+
             Background()
                 .blur(radius: 5)
             
+          // MARK: Formação das teias
             Path { path in
-                let pairs = [(0, 1), (1, 2)]
+                let pairs = [(0, 1), (1, 2), (2, 5), (3,4), (4,5)]
 
                 for (first, second) in pairs {
                     let frame1 = buttonFrames[first]
                     let frame2 = buttonFrames[second]
-                    
-                    if (first, second) == (1, 2) {
-                        // Inverso: de baixo pra cima
+
+                    if (first, second) == (1, 2) || (first, second) == (4, 5) {
                         path.move(to: CGPoint(x: frame2.midX, y: frame2.midY - 40))
                         path.addLine(to: CGPoint(x: frame1.midX, y: frame1.midY - 10))
-                        
+
                         path.move(to: CGPoint(x: frame2.midX , y: frame2.midY - 30))
                         path.addLine(to: CGPoint(x: frame1.midX, y: frame1.midY))
-                        
+
                         path.move(to: CGPoint(x: frame2.midX - 10, y: frame2.midY - 35))
                         path.addLine(to: CGPoint(x: frame1.midX + 10, y: frame1.midY - 8))
-                        
+
                         path.move(to: CGPoint(x: frame2.midX + 20, y: frame2.midY - 30))
                         path.addLine(to: CGPoint(x: frame1.midX + 10, y: frame1.midY - 15))
+
+                    }
+                    else if (first, second) == (2, 5) {
+                        path.move(to: CGPoint(x: frame1.midX - 10, y: frame1.midY))
+                        path.addLine(to: CGPoint(x: frame2.midX - 10, y: frame2.midY))
                         
-                    } else {
-                        // Normal: de cima pra baixo
-                        path.move(to: CGPoint(x: frame1.midX, y: frame1.midY - 40))
-                        path.addLine(to: CGPoint(x: frame2.midX, y: frame2.midY + 10))
-                        
-                        path.move(to: CGPoint(x: frame1.midX , y: frame1.midY - 30))
+                        path.move(to: CGPoint(x: frame1.midX - 10, y: frame1.midY))
                         path.addLine(to: CGPoint(x: frame2.midX, y: frame2.midY))
                         
+                        path.move(to: CGPoint(x: frame1.midX, y: frame1.midY))
+                        path.addLine(to: CGPoint(x: frame2.midX  - 10, y: frame2.midY))
+                        
+                        path.move(to: CGPoint(x: frame1.midX + 5 , y: frame1.midY))
+                        path.addLine(to: CGPoint(x: frame2.midX + 5, y: frame2.midY))
+                        
+                        
+                    }
+                    else {
+                        path.move(to: CGPoint(x: frame1.midX, y: frame1.midY - 40))
+                        path.addLine(to: CGPoint(x: frame2.midX, y: frame2.midY + 10))
+
+                        path.move(to: CGPoint(x: frame1.midX , y: frame1.midY - 30))
+                        path.addLine(to: CGPoint(x: frame2.midX, y: frame2.midY))
+
                         path.move(to: CGPoint(x: frame1.midX + 10, y: frame1.midY - 30))
                         path.addLine(to: CGPoint(x: frame2.midX - 10, y: frame2.midY + 10))
                     }
                 }
             }
-            .stroke(Color.white.opacity(0.8), lineWidth: 2)
+            .stroke(Color.white, lineWidth: 2)
 
             VStack(spacing: 100) {
                 HStack(spacing: 40) {
                     ForEach(0..<3) { index in
+                        ZStack{
                         ButtonComponent()
                             .padding(.bottom, index % 2 == 0 ? 50 : 0)
                             .background {
@@ -66,24 +83,34 @@ struct SettingsView: View {
                                     }
                                 }
                             }
+                            Image(assets[index])
+                                .padding(.bottom, index % 2 == 0 ? 50 : 0)
+                    }
                     }
                 }
-                
+
                 HStack(spacing: 40) {
-                    ForEach(0..<3) { index in
-                        ButtonComponent()
-                            .padding(.bottom, index % 2 == 0 ? 50 : 0)
-                            .background {
-                                GeometryReader { geo in
-                                    Color.clear.onAppear {
-                                        DispatchQueue.main.async {
-                                            buttonFrames[index] = geo.frame(in: .named("canvas"))
+                    ForEach(3..<6) { index in
+                        ZStack{
+                            ButtonComponent()
+                                .padding(.bottom, index % 2 == 0 ? 0 : 50)
+                                .background {
+                                    GeometryReader { geo in
+                                        Color.clear.onAppear {
+                                            DispatchQueue.main.async {
+                                                buttonFrames[index] = geo.frame(in: .named("canvas"))
+                                            }
                                         }
                                     }
                                 }
-                            }
+                            Image(assets[index])
+                                .resizable()
+                                .frame(width: 60, height: 60)
+                                .padding(.bottom, index % 2 == 0 ? 0 : 50)
+                        }
                     }
                 }
+
             }
         }
         .coordinateSpace(name: "canvas")
@@ -94,4 +121,3 @@ struct SettingsView: View {
 #Preview {
     SettingsView()
 }
-
